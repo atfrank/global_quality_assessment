36bd59d6875b5282ebb9b9a377ea2cfe183fd257
Kexin Zhang <kekexin714@gmail.com>
2018-11-28 15:07:14 -0500

    results from different RMSD thresholds; organized directories

---
+++ synthetic_decoys/Kexin/ERT_classifier/scripts/ERT.py
@@ -0,0 +1,107 @@
+import pandas as pd
+import numpy as np
+import sys
+import timeit
+import pickle
+from itertools import compress
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report,confusion_matrix,accuracy_score,recall_score
+from sklearn.ensemble import ExtraTreesClassifier
+
+
+def main():
+  DIR_PATH = '/home/kexin/projects/global_quality_assessment/RF_classifier/'  
+  # LOAD NORMALIZED ERROR MATRIX
+  errors = pd.read_table(DIR_PATH+'errors_normalized.txt',sep=' ')
+  
+  # LEAVE-ONE-OUT ANLAYSIS
+  rnas = ["1XHP", "1YSV", "1Z2J", "1ZC5", "28SR", "2FDT", "2JWV", "2K66", "2KOC", "2L1V", 
+          "2L3E", "2LBJ", "2LBL", "2LDL", "2LDT", "2LHP", "2LI4", "2LK3", "2LP9", "2LPA", 
+          "2LPS", "2LQZ", "2LU0", "2LUB", "2LUN", "2LV0", "2M12", "2M21", "2M22", "2M24", 
+          "2M4W", "2M5U", "2M8K", "2MEQ", "2MHI", "2MNC", "2MXL", "2N2O", "2N2P", "2N4L", 
+          "2N6S", "2N6T", "2N6W", "2N6X", "2NCI", "2QH2", "2QH4", "2RVO", "2Y95", "4A4S", 
+          "4A4T", "4A4U", "5A17", "5A18", "5IEM", "5KH8", "5KMZ", "5KQE", "5LSN", "5LWJ", 
+          "5N5C", "5UF3", "5UZT", "5V16", "5V17", "5WQ1", "6EZ0"]
+          
+  # remove similar sequences
+  sim = pd.read_table(DIR_PATH+'similarity.txt',sep=' ')
+  threshold = 0.80
+  rna = sys.argv[1]
+  print('target RNA: ', rna)
+  fil = sim[rna]>threshold
+  remove_rnas = list(compress(rnas, fil.values))
+  train_rnas = [e for e in rnas if e not in remove_rnas]
+  print('sequences removed: ', remove_rnas)
+  
+  train = errors.loc[errors['id'].isin(train_rnas)]
+  test = errors[errors['id']==rna]
+  
+  # scramble data just to be sure
+  train = train.sample(len(train),random_state=1)
+  test = test.sample(len(test),random_state=1)
+  
+  # make copy of train and test actual flag
+  train_result = train[['id','model','flag']]
+  test_result = test[['id','model','flag']]
+  X_train = train.drop(columns=['id','model','flag','rmsd'])
+  X_test = test.drop(columns=['id','model','flag','rmsd'])
+  y_train = train['flag']
+  y_test = test['flag']
+  
+  del train, test
+  
+  # BUILD CLASSIFIER
+  # grid search parameters
+  n_estimators = [200, 400, 600, 800, 1000]
+  min_samples_split = [2, 5, 10]
+  bootstrap = [True]
+  max_features = ['sqrt', 'log2', None, ]
+  class_weight = ['balanced_subsample', 'balanced', None]
+ 
+  # grid search cv 
+  rf = ExtraTreesClassifier(oob_score = True, verbose = 1)
+  param_grid = dict(n_estimators = n_estimators, min_samples_split = min_samples_split, bootstrap = bootstrap, max_features = max_features, class_weight = class_weight)
+  clf = GridSearchCV(estimator=rf, param_grid=param_grid, cv=5, n_jobs=1, 
+                     return_train_score=True, scoring='accuracy', verbose = 2)
+  grid_result = clf.fit(X_train,y_train)
+  
+  # write log file for neural network information
+  means = grid_result.cv_results_['mean_test_score']
+  stds = grid_result.cv_results_['std_test_score']
+  params = grid_result.cv_results_['params']
+  logfile = DIR_PATH+'log/ert_log_'+rna+'.txt'
+  with open(logfile, 'a') as f:
+    #print("OOB score: %f" % (clf.best_estimator_.oob_score_), file=f)
+    print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_), file=f)
+    for mean, stdev, param in zip(means, stds, params):
+      print("%f (%f) with: %r" % (mean, stdev, param), file=f)
+  
+  # predict and format results
+  y_pred_train = clf.predict(X_train).flatten().tolist()
+  train_result.loc[:,'pred'] = y_pred_train
+  y_pred_test = clf.predict(X_test).flatten().tolist()
+  test_result.loc[:,'pred'] = y_pred_test
+  # print confusion matrix
+  with open(logfile, 'a') as f:
+    print(classification_report(y_test, y_pred_test), file=f)
+    print(confusion_matrix(y_test, y_pred_test), file=f)
+  train_result.to_csv(DIR_PATH + 'pred/ert_train_pred_'+rna+'.txt', sep = ' ', index = False)
+  test_result.to_csv(DIR_PATH + 'pred/ert_test_pred_'+rna+'.txt', sep = ' ', index = False)
+  
+  # save best model
+  best_model = clf.best_estimator_
+  modelname = DIR_PATH + 'models/ert_model_'+rna+'.sav'
+  pickle.dump(best_model, open(modelname, 'wb'))
+  
+if __name__ == "__main__":
+  main()
+                 
+  
+  
+  
+  
+  
+  
+  
---
+++ synthetic_decoys/Kexin/calculate_classification_accuracy.R
@@ -0,0 +1,102 @@
+load_model_accuracy <- function(file = "data/section_1/NN_accuracy_0.6.txt", colnames = c("id", "sen", "spec", "overall"), header = FALSE){
+  data_test <- read.table(file = file, header = T)
+  group1 = get(load(file = "larmord_train_rnas.RData"))
+  group2 = get(load(file = "not_in_larmord.RData"))
+  data_test$type <- "not_in_larmord"
+  data_test$type[data_test$rna %in% group1] <- "in_larmord"
+  data_test$rna <- as.character(data_test$rna)
+  return(data_test)
+}
+
+plot_color_bar <- function(data, metric = "spec"){
+  require(tidyverse)
+  data$group <- data$type
+  data$group <- as.factor(data$group)
+  data$value <- as.numeric(as.character(data[, metric]))
+  data$individual <- data$rna
+  data$rna <- 1:nrow(data)
+  data$colour <- "red"
+  data$colour[data$group == "not_in_larmord"] <- "blue"
+  # Set a number of 'empty bar' to add at the end of each group
+  empty_bar=10
+  to_add = data.frame( matrix(NA, empty_bar*nlevels(data$group), ncol(data)) )
+  colnames(to_add) = colnames(data)
+  to_add$group=rep(levels(data$group), each=empty_bar)
+  data=rbind(data, to_add)
+  data = data %>% arrange(group, value)
+  data$rna=seq(1, nrow(data))
+  # Get the name and the y position of each label
+  label_data=data
+  number_of_bar=nrow(label_data)
+  angle= 90 - 360 * (label_data$rna-0.5) /number_of_bar     # I substract 0.5 because the letter must have the angle of the center of the bars. Not extreme right(1) or extreme left (0)
+  label_data$hjust <- ifelse( angle < -90, 1, 0)
+  label_data$angle<-ifelse(angle < -90, angle+180, angle)
+  
+  # prepare a data frame for base lines
+  base_data=data %>% 
+    group_by(group) %>% 
+    summarize(start=min(rna), end=max(rna) - empty_bar) %>% 
+    rowwise() %>% 
+    mutate(title=mean(c(start, end)))
+  base_data$mean <- mean(data$value[data$group == "not_in_larmord"], na.rm = TRUE)
+  base_data$mean[base_data$group == "in_larmord"] <- mean(data$value[data$group == "in_larmord"], na.rm = TRUE)
+  
+  print(base_data)
+  # prepare a data frame for grid (scales)
+  grid_data = base_data
+  grid_data$end = grid_data$end[ c( nrow(grid_data), 1:nrow(grid_data)-1)] + 1
+  grid_data$start = grid_data$start - 1
+  grid_data=grid_data[-1,]
+  
+  # Make the plot
+  p = ggplot(data, aes(x=as.factor(rna), y=value, fill=data$colour)) +       # Note that rna is a factor. If x is numeric, there is some space between the first bar
+    
+    geom_bar(aes(x=as.factor(rna), y=value, fill=data$colour), stat="identity", alpha=0.5) +
+    
+    # Add a val=100/75/50/25 lines. I do it at the beginning to make sur barplots are OVER it.
+    
+    geom_segment(data=grid_data, aes(x = end, y = 0.80, xend = start, yend = 0.80), colour = "black", alpha=1, size=0.3 , inherit.aes = FALSE ) +
+    geom_segment(data=grid_data, aes(x = end, y = 0.60, xend = start, yend = 0.60), colour = "black", alpha=1, size=0.3 , inherit.aes = FALSE ) +
+    geom_segment(data=grid_data, aes(x = end, y = 0.40, xend = start, yend = 0.40), colour = "black", alpha=1, size=0.3 , inherit.aes = FALSE ) +
+    geom_segment(data=grid_data, aes(x = end, y = 0.20, xend = start, yend = 0.20), colour = "black", alpha=1, size=0.3 , inherit.aes = FALSE ) +
+    geom_segment(data=grid_data, aes(x = end, y = 0.00, xend = start, yend = 0.00), colour = "black", alpha=1, size=0.3 , inherit.aes = FALSE ) +
+    
+    # Add text showing the value of each 100/75/50/25 lines
+    annotate("text", x = rep(max(data$rna), 6), y = c(0.00, 0.20, 0.40, 0.60, 0.80, 1.00), label = c("0.00", "0.20", "0.40", "0.60", "0.80", "1.00") , color="black", size=3.5 , angle=0, fontface="bold", hjust=1) +
+    
+    geom_bar(aes(x=as.factor(rna), y=value, fill=group, colour = "black"), stat="identity", colour = "black") +
+    ylim(-0.5,1.5) +
+    theme_minimal() +
+    theme(
+      legend.position = "none",
+      axis.text = element_blank(),
+      axis.title = element_blank(),
+      panel.grid = element_blank(),
+      plot.margin = unit(rep(0,4), "cm") 
+    ) +
+    coord_polar() +
+    geom_text(data=label_data, aes(x=rna, y=value+0.1, label=individual, hjust=hjust), color="black", fontface="bold",alpha=0.6, size=2.5, angle= label_data$angle, inherit.aes = FALSE ) +
+    
+    # Add base line information
+    geom_segment(data=base_data, aes(x = start, y = 0.5, xend = end, yend = 0.5 ), colour = "red", alpha=0.8, size=0.5 , inherit.aes = FALSE, linetype= 2) +
+    geom_segment(data=base_data, aes(x = start, y = mean, xend = end, yend = mean ), colour = "white", alpha=0.8, size=0.5 , inherit.aes = FALSE, linetype= 2) +
+    geom_segment(data=base_data, aes(x = start, y = 0, xend = end, yend = 0 ), colour = "black", alpha=0.8, size=0.7 , inherit.aes = FALSE )
+  p
+}
+
+# Neural net and ERT accuracy analysis
+acc_NN = load_model_accuracy("Kexin_NN/nn_loo_acc_3.0.txt")
+pdf(file = "Kexin_NN/nn_acc_3.0.pdf", width = 8.44, height = 6.96)
+plot_color_bar(acc_NN, "acc")
+dev.off()
+pdf(file = "Kexin_NN/nn_recall1_3.0.pdf", width = 8.44, height = 6.96)
+plot_color_bar(acc_NN, "recall1")
+dev.off()
+
+acc_ert = load_model_accuracy("Kexin_RF/ert_loo_acc_3.0.txt")
+pdf(file = "Kexin_RF/ert_acc_3.0.pdf", width = 8.44, height = 6.96)
+plot_color_bar(acc_ert, "acc")
+dev.off()
+pdf(file = "Kexin_RF/ert_recall1_3.0.pdf", width = 8.44, height = 6.96)
+plot_color_bar(acc_ert, "recall1")
+dev.off()
