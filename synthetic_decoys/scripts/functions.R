6440484b64e751982288be239bd9756b6a32578c
Kexin Zhang <kekexin714@gmail.com>
2019-03-07 15:20:52 -0500

    recompute error matrices, weighted and unweighted; old error matrices were wrong

---
+++ synthetic_decoys/scripts/functions.R
@@ -0,0 +1,116 @@
+# user function
+norm_errors <- function(errors){
+  # normalizes the errors based on the mean
+  errors$total <- rowSums(errors[,!(colnames(errors) %in% c("id", "model", "reference_flag" ,"rmsd"))], na.rm = TRUE)
+  cols <- colnames(errors)[!(colnames(errors) %in% c("id", "model", "reference_flag","rmsd"))]
+  for (col in cols){
+    #errors[, col] <- errors[, col]/median(errors[, col])
+    errors[, col] <- errors[, col]/median(errors[, col])
+  }
+  return(errors)
+}
+
+get_balanced_set_1 <- function(train, N=10, imbalance=1.0, replace=FALSE){
+  train_true <- subset(train, reference_flag==1)
+  train_false <- subset(train, reference_flag==0)
+  n <- min(nrow(train_false),nrow(train_true))
+  ifelse(n<N, Nlocal <- n, Nlocal <- N)
+  return(rbind(train_false[sample(1:nrow(train_false), Nlocal, replace = replace),], train_true[sample(1:nrow(train_true), floor(Nlocal*imbalance), replace = replace),]))
+}
+
+get_balanced_set_2 <- function(train, ngroups=10, n=20){
+  # get data that even spans rmsd range
+  train <- train[order(train$rmsd),]
+  train$group <- cut(train$rmsd, breaks = ngroups, labels = c(1:ngroups))
+  train <- train[sample(1:nrow(train)),]
+  train <- ddply(.data=train, .variables=c("group"), .fun=function(x)head(x,n))
+  return(train[,!(colnames(train) %in% "group")])
+}
+
+scores <- function(predictions, N=10){
+  # function to quanitfy the ability to correctly classifying models as native or non-native
+  # returns the NSLR and the fraction of the high probability models that are considered native
+  require(nmR)
+  #print(head(predictions))
+  return(data.frame(NSLR=nslr(predictions$reference_flag), RMSD=round(mean(predictions$rmsd[1:N]),3), TOPN=mean(as.numeric(predictions$reference_flag[1:N])-1)))
+}
+
+nslr <- function(X){
+  #' Sum of Logarithmic Ranks Function
+  #'
+  #' This function allows you to compute the normalized sum of logarithmic ranks
+  #' @param X vector of 0 (inactives) and 1 (actives) that was sorted based on some scores (e.g., agreement between measured and predicted shifts)
+  #' @export
+  #' @examples
+  #' random_nslr(sample(c(rep(0,100),rep(1,10))))
+  ri <- which(X==1)
+  N <- length(X)
+  i <-  1:length(ri)
+  SLRmax <- -sum(log(i/N)) # logo rank
+  return(-sum(log(ri/N))/SLRmax)
+} 
+
+
+get_error_matrices <- function(errors, nuclei = c("C1'","C2'","C3'","C4'","C5'","C2","C5","C6","C8","H1'","H2'","H3'","H4'","H5'","H5''","H2","H5","H6","H8"), remove_na = FALSE){
+  # function to write out matrix of chemical shift errors
+  errors <- errors[errors$nucleus %in% nuclei, ]
+  tmp <- expand.grid(unique(errors$model), nuclei, "GUA")
+  tmp <- rbind(tmp, expand.grid(unique(errors$model), nuclei, "ADE"))
+  tmp <- rbind(tmp, expand.grid(unique(errors$model), nuclei, "CYT"))
+  tmp <- rbind(tmp, expand.grid(unique(errors$model), nuclei, "URA"))  
+  colnames(tmp) <- c("model", "nucleus","resname")
+  # colnames order should match errors
+  tmp$resname <- as.character(tmp$resname)
+  tmp <- tmp[order(tmp$model, tmp$nucleus, tmp$resname),]
+  cnames <- unique(paste(tmp$nucleus, tmp$resname, sep=":"))
+  
+  errors <- merge(errors, tmp, all = TRUE)
+  #errors <- ddply(.data = errors, .variables = "id", .fun = function(x){merge(x, tmp, all = T)})
+  errors <- errors[order(errors$model, errors$nucleus, errors$resname), ]  
+  mat <- matrix(errors$V1, byrow = TRUE, ncol = length(cnames))
+  colnames(mat) <- cnames
+  model_info <- unique(errors[,c("model", "rmsd")])
+  #model_info <- ddply(.data = errors, .variables = "id", .fun = function(x){unique(x[,c("model","rmsd")])})
+  model_info <- model_info[complete.cases(model_info),]
+  mat <- cbind(model_info, mat)
+  if(remove_na){mat[is.na(mat)] <- 0}
+  return(mat)
+}
+
+get_error_matrices_test <- function(errors, nuclei = c("C1'","C2'","C3'","C4'","C5'","C2","C5","C6","C8","H1'","H2'","H3'","H4'","H5'","H5''","H2","H5","H6","H8"), remove_na = FALSE){
+  # function to write out matrix of chemical shift errors
+  errors <- errors[errors$nucleus %in% nuclei, ]
+  tmp <- expand.grid(unique(errors$id), nuclei, "GUA")
+  tmp <- rbind(tmp, expand.grid(unique(errors$id), nuclei, "ADE"))
+  tmp <- rbind(tmp, expand.grid(unique(errors$id), nuclei, "CYT"))
+  tmp <- rbind(tmp, expand.grid(unique(errors$id), nuclei, "URA"))  
+  colnames(tmp) <- c("id", "nucleus","resname")
+  cnames <- gsub("\'","p",unique(paste(tmp$nucleus, tmp$resname, sep="")))
+  errors <- merge(errors, tmp, all = TRUE)
+  errors <- errors[order(errors$id, errors$nucleus, errors$resname), ]  
+  mat <- matrix(errors$V1, byrow = TRUE, ncol = length(cnames))
+  colnames(mat) <- cnames
+  if(remove_na){mat[is.na(mat)] <- 0}
+  return(mat)
+}
+
+prep_data_test <- function(expcs_file, predcs_file, rna = "2LPS", errors_file){
+  # read in data
+  rmsd <- read.table("../../data/rmsd_info", col.names = c("id", "model", "rmsd"), stringsAsFactors = F)
+  rmsd <- rmsd[grepl(rna, rmsd$model),]
+  predcs <- nmR::load_cs_data(csfile = predcs_file, accuracyFile = "data/larmord_accuracy_resname_nucleus.txt", atomBasedWeights = FALSE, names = c("id", "resid", "resname", "nucleus", "predCS", "model"))
+  expcs <-  nmR::load_cs_data(csfile = expcs_file, names = c("resname", "resid", "nucleus", "expCS", "error"))
+  
+  # merge
+  cs <- merge(predcs, rmsd, by = c("model","id"))
+  cs <- merge(expcs, cs, by = c("resname", "resid", "nucleus"))
+  cs$flag <- 0
+  cs$flag[cs$rmsd < 3] <- 1
+
+  # get error matrix
+  errors <- plyr::ddply(.dat=cs, .var=c("model","flag", "rmsd", "id", "resname", "nucleus"), .fun=nmR::score_mae)
+  errors <- plyr::ddply(.dat=errors, .var=c("id"), .fun = get_error_matrices_test)
+
+  # save data
+  write.table(errors, errors_file, col.names = T, row.names = F, quote = F)
+}
---
+++ synthetic_decoys/scripts/prep_error_matrix.R
@@ -0,0 +1,31 @@
+setwd("~/documents/github/global_quality_assessment/synthetic_decoys")
+source("scripts/functions.R")
+
+# read in rmsd file
+rmsd <- read.table("rmsd_files/all.txt", col.names = c("id","model","rmsd"))
+
+# read in larmord predicted shifts
+predcs <- nmR::load_cs_data(csfile = "chemical_shift_files/all.txt", names = c("id", "resid", "resname", "nucleus", "predCS", "model"))
+
+# read in larmord corrected observed shifts
+expcs <-  nmR::load_cs_data(csfile="observed_shifts_corrected/all_measured_corrected.txt", names = c("id", "resname", "resid", "nucleus", "expCS", "error"))
+
+# merge
+cs <- merge(predcs, rmsd, by = c("model","id"))
+cs <- merge(expcs, cs, by = c("resname","resid","nucleus","id"))
+# unweighted errors
+cs$weight <- 1
+
+# get error matrices
+errors <- plyr::ddply(.dat=cs, .var=c("model", "rmsd", "id", "resname", "nucleus"), .fun=nmR::score_mae)
+errors <- plyr::ddply(.dat=errors, .var=c("id"), .fun = get_error_matrices)
+
+# save
+save(cs, file = "data/chemical_shifts.RData")
+save(errors, file = "error_matrix_unweighted.RData")
+
+predcs2 <- nmR::load_cs_data(csfile="chemical_shift_files/all.txt", accuracyFile = "data/larmord_accuracy_resname_nucleus.txt", atomBasedWeights = FALSE, names = c("id", "resid", "resname", "nucleus", "predCS", "model"))
+cs2 <- merge(predcs2, rmsd, by = c("model","id"))
+cs <- merge(expcs, cs2, by = c("resname","resid","nucleus","id"))
+errors <- plyr::ddply(.dat=cs, .var=c("model", "rmsd", "id", "resname", "nucleus"), .fun=nmR::score_mae)
+errors <- plyr::ddply(.dat=errors, .var=c("id"), .fun = get_error_matrices)
